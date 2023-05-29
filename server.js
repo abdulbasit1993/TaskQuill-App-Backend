cdf5dee00b1fd4c0a31767bcf955c814a1c3a662
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-05-29 12:07:35 +0500

    Added Swagger API Docs

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const app = express();
 
 const authRoutes = require('./routes/auth')
@@ -14,6 +17,8 @@ let corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(bodyParser.json());
 
