e9543e0f63f58948b551f5211b35e77ef863063c
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-06-02 15:21:57 +0500

    Fixed User Profile API controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,39 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const Role = require("../models/role");
 
 const getUserProfile = async (req, res) => {
   try {
     const authHeader = req.headers.authorization;
     const token = authHeader.split(" ")[1];
-
     const decodedToken = jwt.verify(token, process.env.JWTSECRET);
+    const userId = decodedToken?.userId;
 
-    let extractedData = {
-      id: decodedToken?.id,
-      username: decodedToken?.username,
-      email: decodedToken?.email,
-      roles: decodedToken?.roles.map((role) => role.name),
+    let roles = [];
+
+    const user = await User.findById(userId);
+
+    const roleId = user?.roles;
+
+    try {
+      const role = await Role.findById(roleId);
+      roles = [...roles, role?.name];
+    } catch (err) {
+      console.log(err);
+    }
+
+    let userData = {
+      _id: user?._id,
+      username: user?.username,
+      email: user?.email,
+      roles: roles,
+      createdAt: user?.createdAt,
+      updatedAt: user?.updatedAt,
     };
 
     res.status(200).json({
       success: true,
-      data: extractedData,
+      data: userData,
     });
   } catch (err) {
     return res.status(500).json({
