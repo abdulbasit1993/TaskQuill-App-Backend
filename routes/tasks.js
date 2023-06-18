53ac332b409425b19e407700ccca3508886f0256
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-06-19 01:19:03 +0500

    Delete Task API completed

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const {authJwt} = require('../middlewares');
-const taskController = require('../controllers/tasks');
+const express = require("express");
+const { authJwt } = require("../middlewares");
+const taskController = require("../controllers/tasks");
 const router = express.Router();
 
 router.post("/tasks/add", [authJwt.verifyToken], taskController.addTask);
 
 router.get("/tasks/getAll", [authJwt.verifyToken], taskController.getTasks);
 
-module.exports = router;
+router.delete(
+  "/tasks/delete/:id",
+  [authJwt.verifyToken],
+  taskController.deleteTask
+);
 
+module.exports = router;
