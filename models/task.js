03ff9e0d68a3b58a307c9dad71d53cd7a5ccaecd
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-06-26 00:44:21 +0500

    Completed Update Task APIs

--- models/task.js
+++ models/task.js
@@ -1,35 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
+const TaskSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     dueDate: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     status: {
-        type: String,
-        enum: ['completed', 'incomplete'],
-        default: 'incomplete'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+      type: String,
+      enum: ["completed", "incomplete"],
+      default: "incomplete",
     },
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
-});
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const Task = mongoose.model("Task", TaskSchema);
 
